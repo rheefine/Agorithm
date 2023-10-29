d8261d1f7d39b6610b8dc0c38080cb597c6eaafc
Rhee DongHyun <rheefine@gmail.com>
2023-10-29 17:03:01 +0900

    [Gold V] Title: 동전 2, Time: 372 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,17 @@
+# 2294 동전 2
+
+n, k = map(int, input().split())
+
+coins = [int(input()) for _ in range(n)]
+
+dp = [10001] * (k + 1)
+dp[0] = 0
+
+for coin in coins:
+   for i in range(coin, k+1):
+       dp[i] = min(dp[i], dp[i-coin]+1)
+
+if dp[k] == 10001:
+   print(-1)
+else:
+   print(dp[k])
